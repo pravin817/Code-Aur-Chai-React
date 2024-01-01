2a059595712625d7d155d31530890ab611aac5f3
Pravin Mhaske <pravinmhaske817@gmail.com>
2024-01-01 08:32:31 +0530

    Interview Question

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -8,7 +8,22 @@ function App() {
     if (count >= 20) {
       alert("Sorry You can't increase the value of count more than 20.");
     } else {
-      setCount(count + 1);
+      // setCount(count + 1);
+      // setCount(count + 1);
+      // setCount(count + 1);
+      // setCount(count + 1);
+
+      /*
+      Note : The above senario will update the value of the count by only one, because the useState hook works in the batches, it will consider it as one batch.
+      */
+
+      // If we want to change the value of coun each time we need to follow the following syntax.
+      setCount((preCount) => preCount + 1);
+      setCount((preCount) => preCount + 1);
+      setCount((preCount) => preCount + 1);
+      setCount((preCount) => preCount + 1);
+
+      
     }
   };
 
@@ -24,8 +39,8 @@ function App() {
     <>
       <h1>Code Aur React</h1>
       <h2>Counter : {count}</h2>
-      <button onClick={IncreaseCount}>Increase Count : {count}</button>
-      <button onClick={DecraseCount}>Decrease Count : : {count}</button>
+      <button onClick={IncreaseCount}>Increase Count :: {count}</button>
+      <button onClick={DecraseCount}>Decrease Count :: {count}</button>
       <p>Footer : {count}</p>
     </>
   );
