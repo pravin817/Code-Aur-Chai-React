fd1ae75be768c7e0d4217af4a128bba1a4c30db7
Pravin Mhaske <pravinmhaske817@gmail.com>
2024-01-05 09:13:21 +0530

    Appwrite service added.

---
+++ 12BlogMega/src/appwrite/auth.js
@@ -0,0 +1,68 @@
+import config from "../config/config";
+
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(config.appwriteUrl)
+      .setProject(config.appwriteProjectId);
+
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+
+      // If the account is created then redirect to login
+      if (userAccount) {
+        // call another method
+        // return userAccount;
+        return this.login(email, password);
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("Appwrite service :: getCurrentUser :: error ", error);
+    }
+
+    return null;
+  }
+
+  async logout() {
+    try {
+      await this.account.deleteSessiona();
+    } catch (error) {
+      console.log("Appwrite service :: logout error ", error);
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
