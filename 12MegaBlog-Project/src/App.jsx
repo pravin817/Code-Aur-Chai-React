0a3cda0319572c19c891c000f38eaa019552e306
Pravin Mhaske <pravinmhaske817@gmail.com>
2024-01-17 16:17:29 +0530

    Mega Project - Blog Website configuration done successfully.

---
+++ 12MegaBlog-Project/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  console.log("Appwrite", import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <>
+      <h1>A Blog Project with Appwrite</h1>
+    </>
+  );
+}
+
+export default App;
