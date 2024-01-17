dc49170ec24f5f77e08ab0bdc8006abe9f43c471
Pravin Mhaske <pravinmhaske817@gmail.com>
2024-01-17 16:10:30 +0530

    project rename done

--- reduxToolkit/src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-// now provide the reducers to the store
-
-import todoReducer from "../features/todo/todoSlice";
-
-export const store = configureStore({
-  reducer: todoReducer,
-});
